38594de20eacbb8c6cc62901230c5ea826fb306b
Anthony <anthonyumbriac@gmail.com>
2021-12-02 11:12:10 -0800

    Update trending.js
    Minor bugfix for key prop

--- src/pages/trending.js
+++ src/pages/trending.js
@@ -11,8 +11,8 @@ const Trending = () => {
                 <h1 className="mt-3">TRENDING <span className="material-icons text-danger fs-1">local_fire_department</span></h1>
                 {state.trending &&
                     state.trending.map((coin, idx) => (
-                        <div className={`col-${idx === 0 ? 0 : 2} mb-3`}>
-                            <div className={`p-3`} key={idx} style={idx === 0 ? { background: 'transparent'} : { background: '#000'}}>
+                        <div key={idx} className={`col-${idx === 0 ? 0 : 2} mb-3`}>
+                            <div className={`p-3`} style={idx === 0 ? { background: 'transparent'} : { background: '#000'}}>
                                 <span className={`badge mx-auto mb-2 ${idx === 0 ? 'bg-danger' : 'bg-primary'}`}>#{idx + 1}</span>
                                 <img alt={coin.item.name} style={idx === 0 ? { width: '10rem', height: '10rem' } : { width: '5rem', height: '5rem' }} src={coin.item.large} 
                                     className={`img-fluid rounded mx-auto m-2 d-block ${idx === 0 ? 'pulse' : ''}`} />
